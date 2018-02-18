b417d88f448b4fc2c54d9c9a93e8cd9ac0669434
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-02-18 15:58:45 -0300

    header to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import CoderockrLogo from './assets/images/hand-yellow.svg'
 import Home from './Home'
+import Footer from './Footer';
 import { Switch, Route } from 'react-router-dom'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+  <div className="CWSPApp">
+    <header>
+      <a href="/">
+        <CoderockrLogo className="logo" />
+        <h1 className="text">CODEROCKR WAY PROJECT SETUP</h1>
+      </a>
+    </header>
+    <div className=" container">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+    <Footer />
+  </div>
 )
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Footer from './Footer';
 import { withRouter } from 'react-router-dom';
 
-import CoderockrLogo from './assets/images/hand-yellow.svg'
 import githubLogo from './assets/images/github.svg'
 import gitlabLogo from './assets/images/gitlab.svg'
 import trelloLogo from './assets/images/trello.svg'
@@ -32,38 +30,32 @@ const SETUPABLE = [
 
 const Home = () => (
   <div className="Home">
-    <header>
-      <CoderockrLogo className="logo" />
-      <h1 className="text">CODEROCKR WAY PROJECT SETUP</h1>
-    </header>
-    <div className="container">
-      <section className="description">
-        <p>
-          This web client helps to setup a project at GitHub, GitLab or Trello to use the
-          Coderockr Way's labels, making easier to bootstrap the basic configuration at
-          your project.
-          </p>
-      </section>
-      <section className="project-handlers">
-        <h2>Where is your project?</h2>
-        <ul className="row project-handlers-buttons justify-content-md-center">
-          {SETUPABLE.map(({ name, logo: Logo, url, enabled }) => (
-            <li key={name} className="col-md-4">
-              <a className={`btn btn-primary ${enabled ? null : 'disabled'}`} href={url}
-                alt={enabled ? '' : `${name} will be supported soon`}>
-                <Logo className="icon" />
-                {name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section className="more-about">
-        <h4>Wanna know more about?</h4>
-        <p>Look this link: <a href="https://blog.coderockr.com/simplificando-o-setup-de-projetos-no-github-f29b76c83194">Simplificando o Setup de Projetos no GitHub</a>
-        </p>
-      </section>
-    </div>
+    <section className="description">
+      <p>
+        This web client helps to setup a project at GitHub, GitLab or Trello to use the
+        Coderockr Way's labels, making easier to bootstrap the basic configuration at
+        your project.
+      </p>
+    </section>
+    <section className="project-handlers">
+      <h2>Where is your project?</h2>
+      <ul className="row project-handlers-buttons justify-content-md-center">
+        {SETUPABLE.map(({ name, logo: Logo, url, enabled }) => (
+          <li key={name} className="col-md-4">
+            <a className={`btn btn-primary ${enabled ? null : 'disabled'}`} href={url}
+              alt={enabled ? '' : `${name} will be supported soon`}>
+              <Logo className="icon" />
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+    <section className="more-about">
+      <h4>Wanna know more about?</h4>
+      <p>Look this link: <a href="https://blog.coderockr.com/simplificando-o-setup-de-projetos-no-github-f29b76c83194">Simplificando o Setup de Projetos no GitHub</a>
+      </p>
+    </section>
   </div>
 )
 
