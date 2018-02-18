1fd459cfb59db05ead76b5ca50d3868a7cd68202
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-02-18 15:58:53 -0300

    Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,49 +1,13 @@
 import React from 'react';
-import whiteHand from './assets/images/hand-white.svg';
+import CoderockrWhite from './assets/images/hand-white.svg';
+import './Footer.css'
 
 const Footer = () => (
   <footer id="footer">
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <ul className="social">
-            <li>
-              <a href="https://www.facebook.com/coderockr" title="Curta a Coderockr no Facebook">
-                <span className="icon-facebook"></span>
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/coderockr" title="Siga a Coderockr no Twitter"
-                target="_blank" rel="noopener noreferrer">
-                <span className="icon-twitter"></span>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/user/coderockr" title="Acompahe a Coderockr no YouTube"
-                target="_blank" rel="noopener noreferrer">
-                <span className="icon-youtube"></span>
-              </a>
-            </li>
-            <li>
-              <a href="https://blog.coderockr.com" title="Leia sore a Coderockr no Medium"
-                target="_blank" rel="noopener noreferrer">
-                <span className="icon-medium"></span>
-              </a>
-            </li>
-            <li>
-              <a href="https://plus.google.com/+Coderockr" title="Acompahe a Coderockr no Google+"
-                target="_blank" rel="noopener noreferrer">
-                <span className="icon-google-plus"></span>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/Coderockr" title="Acompahe a Coderockr no Github"
-                target="_blank" rel="noopener noreferrer">
-                <span className="icon-github"></span>
-              </a>
-            </li>
-          </ul>
-          <img src={whiteHand} alt="Coderockr Logo" title="You Rock!" className="pull-right" />
+          <CoderockrWhite alt="Coderockr Logo" title="You Rock!" className="white-logo pull-right" />
           <div className="text">
             <p>
               +55 47 3227-6359 &nbsp;&nbsp;&nbsp;
