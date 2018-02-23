3ffd7c7f64b8f08f58d1d85cbf2a2cb80c9a420a
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-02-22 22:37:55 -0300

    base

--- src/GitHub/GitHub.js
+++ src/GitHub/GitHub.js
@@ -3,6 +3,9 @@ import Loading from '../Components/Loading';
 import { withGithubRepositories } from './withGitHubRepositories';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+import LABELS_TO_ADD from '../Labels';
+import invertColor from '../invertColor';
+import './GitHub.css';
 
 class GitHub extends React.Component {
 
@@ -29,14 +32,33 @@ class GitHub extends React.Component {
           </section>
           :
           <section>
-            <Select
-              value={selectedOption}
-              onChange={(e) => this.handleSelect(e)}
-              options={repositories.map(r => Object.assign(r, {
-                value: r.full_name,
-                label: r.full_name,
-              }))}
-            />
+            <div className="row select-group">
+              <div className="col-md-10">
+                <Select
+                  value={selectedOption}
+                  onChange={(e) => this.handleSelect(e)}
+                  options={repositories.map(r => Object.assign(r, {
+                    value: r.full_name,
+                    label: r.full_name,
+                  }))}
+                />
+              </div>
+              <div className="col-md-2">
+                <button className="btn btn-apply-labels btn-primary">Apply</button>
+              </div>
+            </div>
+            <div className="row labels">
+              {LABELS_TO_ADD.map(({ name, color }) => (
+                <div key={name} className="col-md-4">
+                  <label alt={name} className="label-item"
+                    style={{ backgroundColor: '#' + color, color: invertColor('#' + color, true) }}
+                  >
+                    <input disabled type="checkbox" />
+                    {name}
+                  </label>
+                </div>
+              ))}
+            </div>
           </section>
         }
       </div>
