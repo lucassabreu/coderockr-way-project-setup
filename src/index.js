12923bb1fdf5922c845612ddfbe717eb27a4b4c8
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-02-26 10:38:00 -0300

    use local material-icons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'material-design-icons/iconfont/material-icons.css'
+
 ReactDOM.render(
   <Router><App /></Router>,
   document.getElementById('root')
